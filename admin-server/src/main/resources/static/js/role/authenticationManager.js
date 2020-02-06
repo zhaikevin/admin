de008b09b6f3602cf29abdc21f04442b7f5b8a09
zhaijizhong <863551278@qq.com>
2020-02-06 21:05:32 +0800

    权限管理-选择逻辑

--- admin-server/src/main/resources/static/js/role/authenticationManager.js
+++ admin-server/src/main/resources/static/js/role/authenticationManager.js
@@ -52,6 +52,40 @@ var app = new Vue({
                 self.$message.error('获取菜单异常')
             });
         },
+        clickMenu (currentObj, treeStatus) {
+            // 用于：父子节点严格互不关联时，父节点勾选变化时通知子节点同步变化，实现单向关联。
+            var selected = treeStatus.checkedKeys.indexOf(currentObj.code) // -1未选中
+            // 选中
+            if (selected !== -1) {
+                // 子节点只要被选中父节点就被选中
+                this.selectedParent(currentObj)
+                // 统一处理子节点为相同的勾选状态
+                this.uniteChildSame(currentObj, true)
+            } else {
+                // 未选中 处理子节点全部未选中
+                if (currentObj.children != undefined && currentObj.children.length !== 0) {
+                    this.uniteChildSame(currentObj, false)
+                }
+            }
+        },
+        // 统一处理子节点为相同的勾选状态
+        uniteChildSame (currentObj, isSelected) {
+            this.$refs.tree.setChecked(currentObj.code, isSelected)
+            if (currentObj.children != undefined && currentObj.children.length !== 0) {
+                for (var i = 0; i < currentObj.children.length; i++) {
+                    this.uniteChildSame(currentObj.children[i], isSelected)
+                }
+            }
+        },
+        // 统一处理父节点为选中
+        selectedParent (currentObj) {
+            var currentNode = this.$refs.tree.getNode(currentObj)
+            if (currentNode.parent.key !== undefined) {
+                this.$refs.tree.setChecked(currentNode.parent, true)
+                this.selectedParent(currentNode.parent)
+            }
+        },
+
         save(){
             var nodes = this.$refs.tree.getCheckedNodes(false,true)
             console.log(nodes)
